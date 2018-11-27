025d7417365414d1383d6f3712f86e32b7336613
pedroferreiramamede <pedroferreira.mamede@gmail.com>
2018-11-27 01:22:18 -0200

    fixed some identations

--- testeSln.py
+++ testeSln.py
@@ -7,50 +7,11 @@ from selenium.webdriver.support import expected_conditions as EC
 
 import sys
 import time
-import threading
 from termcolor import colored
 
-class Spinner:
-    busy = False
-    delay = 0.1
-
-    @staticmethod
-    def spinning_cursor():
-        while 1: 
-            for cursor in '|/-\\': yield cursor
-
-    def __init__(self, delay=None):
-        self.spinner_generator = self.spinning_cursor()
-        if delay and float(delay): self.delay = delay
-
-    def spinner_task(self):
-        while self.busy:
-            sys.stdout.write(next(self.spinner_generator))
-            sys.stdout.flush()
-            time.sleep(self.delay)
-            sys.stdout.write('\b')
-            sys.stdout.flush()
-
-    def start(self):
-        self.busy = True
-        threading.Thread(target=self.spinner_task).start()
-
-    def stop(self):
-        self.busy = False
-        time.sleep(self.delay)
-
-spinner = Spinner()
-print 'Aguarde um segundo '
-
-spinner.start()
 driver=webdriver.Firefox()
 driver.minimize_window()
 
-spinner.stop()
-sys.stdout.flush()
-#sys.stdout.write("\033[F") #back to previous line
-sys.stdout.write("\033[K") #clear line
-#spinner.stop()
 searchVideo = str(raw_input("Busca: "))
 
 #driver.get('https://github.com/')
@@ -122,7 +83,7 @@ while appPlaying:
             sys.stdout.write("\033[F") #back to previous line
             sys.stdout.write("\033[K") #clear line
         try:
-            driver.find_element_by_class_name('ytp-ad-skip-button.ytp-button').click();
+            driver.find_element_by_class_name('ytp-ad-skip-button.ytp-button').click()
             print 'ad skiped'
             sys.stdout.write("\033[F") #back to previous line
             sys.stdout.write("\033[K") #clear line
@@ -132,19 +93,18 @@ while appPlaying:
             sys.stdout.write("\033[K") #clear line   
     if actionOnPlaying == 'p':
         if musicPlaying:
-            print '||'
-            driver.find_element_by_class_name('ytp-play-button.ytp-button').click()
-            sys.stdout.write("\033[F") #back to previous line
-            sys.stdout.write("\033[K") #clear line
+	        print '||'
+	        driver.find_element_by_class_name('ytp-play-button.ytp-button').click()
+	        sys.stdout.write("\033[F") #back to previous line
+	        sys.stdout.write("\033[K") #clear line
             musicPlaying = False
         else:
             print '>'
-            driver.find_element_by_class_name('ytp-play-button.ytp-button').click()
-            sys.stdout.write("\033[F") #back to previous line
-            sys.stdout.write("\033[K") #clear line
+	        driver.find_element_by_class_name('ytp-play-button.ytp-button').click()
+	        sys.stdout.write("\033[F") #back to previous line
+	        sys.stdout.write("\033[K") #clear line
             musicPlaying = True
     if actionOnPlaying == 'q':
         print 'exiting...'
         driver.quit()
         appPlaying = False
-
